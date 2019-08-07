38c7274450370abfb5215772ffc6d81118a36cfd
Forrest JB Hartley <forrest@forrestbear.org>
2019-08-07 10:03:49 -0400

    added py script to display status of gz-verify

---
+++ gz-verify/src/gzanalyze.py
@@ -0,0 +1,26 @@
+'''
+Declare list which will contain lists.
+Get list of files in directory.
+Iterate over them.
+Get values and insert them in a list.
+Append that list to a list of lists you declared at the top.
+Outside of loop Convert the list of lists to a dataframe with pd.DataFrame(list_of_lists)
+'''
+
+import os
+import glob
+import pandas as pd
+
+filenames = []
+passed = []
+basepath = '/Users/forrest/testing/'
+for filename in glob.glob(os.path.join(basepath, '*.vfy')):
+    filenames.append(filename)
+    with open(filename, 'r') as f:
+        lineList = f.readlines()
+        passed.append(lineList[-1])
+
+d = {"filenames": filenames, "passed": passed}
+df = pd.DataFrame(d)
+
+print(df)
