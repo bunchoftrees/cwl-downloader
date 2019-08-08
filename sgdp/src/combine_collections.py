293a05634fc9f64890ab350d0ed616d438010c83
Forrest Hartley <bunchoftrees@users.noreply.github.com>
2019-08-08 11:14:41 -0400

    Update combine_collections.py

--- sgdp/src/combine_collections.py
+++ sgdp/src/combine_collections.py
@@ -1,8 +1,8 @@
 import arvados
 import arvados.collection
 api = arvados.api()
-project_uuid = "su92l-j7d0g-564z7zxkttp3vih"
-collection_uuids = ["su92l-4zz18-c3wnxzg56doxaxv", "su92l-4zz18-c7f9t14zqmthlx2"]
+project_uuid = "su92l-uuid-project"
+collection_uuids = ["su92l-collection1", "su92l-collection2"]
 combined_manifest = ""
 for u in collection_uuids:
     c = api.collections().get(uuid=u).execute()
